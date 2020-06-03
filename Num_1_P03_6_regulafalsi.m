4d3ab18a5036b5805f3272fb4d4eedb5a4526c58
Attila Kékesi <65607182+akekesi@users.noreply.github.com>
2020-06-03 15:11:12 +0200

    Correct func_name

--- Num_1_P03_6_regulafalsi.m
+++ Num_1_P03_6_regulafalsi.m
@@ -1,5 +1,5 @@
 %% Regula-Falsi Verfahren (f stetig, f(a)<0 und f(b)>0)
-function [x, steps] = Num_1_P03_regulafalsi(f,a,b,tol)
+function [x, steps] = Num_1_P03_6_regulafalsi(f,a,b,tol)
 d = 111;
 x_0 = a;
 steps = 0;
